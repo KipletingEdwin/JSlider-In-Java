6d7f4615fe6a38e76bcdf4c6b440753ee6314c35
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-14 09:06:28 +0300

    finalize instanctiating the JFrame, JPanel,JLabel, and JSlider

--- src/SliderDemo.java
+++ src/SliderDemo.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -5,6 +6,19 @@ import javax.swing.event.ChangeListener;
 
 public class SliderDemo implements ChangeListener {
 
+    JFrame frame;
+    JPanel panel;
+    JLabel label;
+    JSlider slider;
+
+
+    SliderDemo(){
+        frame = new JFrame();
+        panel = new JPanel();
+        label = new JLabel();
+        slider = new JSlider();
+
+    }
 
 
     @Override
