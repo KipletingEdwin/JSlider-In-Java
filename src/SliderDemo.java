99b5f980201dbc5b1c22ab11dfb6bed718d72d5b
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-14 09:01:26 +0300

    add a SliderDemo class that implements ChangeListener

---
+++ src/SliderDemo.java
@@ -0,0 +1,14 @@
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
+
+public class SliderDemo implements ChangeListener {
+
+
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+
+    }
+}
